e78760ce92700f39d5d9d569c3b1412168c8806e
mattmikemo <mattmikemo@gmail.com>
2024-04-29 00:14:41 -0500

    Integrated first react component and ui systems

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,7 @@
+const {
+  default: flattenColorPallete,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -16,9 +20,22 @@ module.exports = {
     // ...
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  darkMode: "class",
+ plugins: [addVariablesForColors],
   plugins: [nextui()]
-}
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+ function addVariablesForColors({ addBase, theme }: any) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+  
+    addBase({
+     ":root": newVars,
+   });
+  } 
