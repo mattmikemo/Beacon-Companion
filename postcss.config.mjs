259d05cc6200dcf7dbf4cbe09c32c19af7de4cc0
mattmikemo <mattmikemo@gmail.com>
2024-05-23 13:21:16 -0500

    Added new Text Generate component

---
+++ postcss.config.mjs
@@ -0,0 +1,26 @@
+const autoprefixer = require('autoprefixer');
+const postcssPresetEnv = require('postcss-preset-env');
+const csso = require('postcss-csso');
+
+const config = {
+	plugins: [
+		postcssPresetEnv({
+			stage: 3,
+			features: {
+				'nesting-rules': true,
+				'custom-media-queries': true,
+				'media-query-ranges': true,
+			},
+		}),
+		autoprefixer(),
+		csso(),
+	],
+};
+
+module.exports = {
+    plugins: [
+      require('tailwindcss'),
+      require('@tailwindcss/deprecation-warnings'),
+      require('autoprefixer'),
+    ],
+  };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,41 +1,32 @@
 const {
-  default: flattenColorPallete,
+  default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
-
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
-
-// tailwind.config.cjs
-const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // ...
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
     extend: {},
   },
- plugins: [addVariablesForColors],
-  plugins: [nextui()]
+  plugins: [addVariablesForColors],
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
- function addVariablesForColors({ addBase, theme }: any) {
-    let allColors = flattenColorPalette(theme("colors"));
-    let newVars = Object.fromEntries(
-      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-    );
-  
-    addBase({
-     ":root": newVars,
-   });
-  } 
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
