3e3d989f40f884a074dadbffffb8075af2951e77
mattmikemo <mattmikemo@gmail.com>
2024-08-23 19:57:20 -0500

    Added additonal props for the card and added another table to Astro DB

--- db/config.ts
+++ db/config.ts
@@ -1,18 +1,23 @@
 import { defineDb, defineTable, column } from 'astro:db';
 
-const Author = defineTable({
+const StandardAction = defineTable({
   columns: {
-    id: column.number(),
-    name: column.text(),
-  }
-});
-
-const Comment = defineTable({
-  columns: {
-    authorId: column.number(),
-    body: column.text(),
+    Id: column.number(),
+    created_at: column.date(),
+    title: column.text(),
+    phase_number: column.text(),
+    phase_name: column.text(),
+    standard_action_description: column.text(),
+    modifier: column.text(),
+    standard_action_condition: column.text(),
+    description_phase_title: column.text(),
+    standard_action_tag: column.text(),
+    spending_cost: column.text(),
+    limiter_description: column.text(),
+    ability_parameter: column.text(),
+    footer_specifier: column.text(),
   }
 });
 export default defineDb({
-  tables: { Comment, Author },
+  tables: { StandardAction },
 })
